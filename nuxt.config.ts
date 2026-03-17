0a140213282a35fcf99d64fc5c1de1056f1c94a2
indie9 <96477129+indie9@users.noreply.github.com>
2026-03-17 21:55:03 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  // Pinia подключаем вручную через плагин, чтобы обойти баг SSR-хука app:rendered
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   supabase: {
-    redirect: false
+    redirect: false,
+    // ЯВНО пробрасываем значения из env
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_ANON_KEY,
+    serviceKey: process.env.SUPABASE_SERVICE_ROLE_KEY
   }
 })
