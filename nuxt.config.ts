4cd213732e83ecef564d1b6b29014c230d89a63d
Nikolay E. Galanov <n.galanov@kck.ru>
2026-07-08 23:08:13 +0300

    feat: add integration logs 7

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
+  nitro: {
+    preset: 'node-server',
+  },
   // Pinia подключаем вручную через плагин, чтобы обойти баг SSR-хука app:rendered
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   // Автоимпорт стооров из папки stores, чтобы можно было использовать
